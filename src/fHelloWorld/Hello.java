bbe6036f8933a1d26e7a495578e8f560c90ce60a
Moreno <roberto.moreno@epicor.com>
2020-01-25 11:50:00 -0600

    Agregando algo

--- src/fHelloWorld/Hello.java
+++ src/fHelloWorld/Hello.java
@@ -2,7 +2,8 @@ package fHelloWorld;
 
 public class Hello {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) 
+	{
 		System.out.println("C#>Java\n");
 		System.out.println("IntelliJ>Eclipse\n");
 		System.out.println("I hate java\n");
