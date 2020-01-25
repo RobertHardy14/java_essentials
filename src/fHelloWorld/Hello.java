0b19c1e6e49d247eb11b26fa76d97fd896a183b6
Moreno <roberto.moreno@epicor.com>
2020-01-25 09:58:02 -0600

    Agregando cambios a archivo HelloWorld y Sandwich

--- src/fHelloWorld/Hello.java
+++ src/fHelloWorld/Hello.java
@@ -3,6 +3,8 @@ package fHelloWorld;
 public class Hello {
 
 	public static void main(String args[]) {
-		System.out.println("hello, world\n");   
+		System.out.println("C#>Java\n");
+		System.out.println("IntelliJ>Eclipse\n");
+		System.out.println("I hate java\n");
 	}
 }
